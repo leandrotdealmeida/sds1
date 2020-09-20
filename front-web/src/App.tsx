1dbd0aaa7420ba5ae70ebf8477118f3c1f897b9f
Leandro Almeida <leandrotrovilho@gmail.com>
2020-09-20 15:47:05 -0300

    Criando navegação com rotas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
+import Routes from './Routex';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Home />
+      <Routes />
     </div>
   );
 }
